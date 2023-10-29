ad4fbe5d71a8cd1674618134cb407d53818623e5
nepalankit <nepalankit16@gmail.com>
2023-10-29 10:28:49 +0545

    scarpind datas

--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -1,10 +1,50 @@
 "use client"
 
-import React from 'react'
+import React, { FormEvent } from 'react'
+import {useState} from 'react'
+import { scrapeAndStoreProduct } from '../lib/actions'
+const isValidAmazonProductLink=(url:string)=>{
+    try{
+        const parsedURL=new URL(url)
+        const hostname=parsedURL.hostname
+        if(
+            hostname.includes('amazon.com') ||
+            hostname.includes('amazon.') ||
+            hostname.endsWith('amazon')
+            
+        ){
+            return true;
+        }
+    }
+    catch(error)
+    {
+        return false
+    }
+
+
+}
 
 const Searchbar = () => {
-    const handleSubmit=()=>{
-        alert('You searched for:')
+    const [searchPrompt, setSearchPrompt]=useState('');
+    const [isLoading, setIsLoading]=useState(false);
+    const handleSubmit=async (event:FormEvent<HTMLFormElement>)=>{
+        event.preventDefault()
+        const isValidLink=isValidAmazonProductLink(searchPrompt)
+   
+        alert(isValidLink ? 'valid link':'Invalid Link')
+        if(!isValidLink)
+        return alert('Please Provide a valid Amazon Link')
+    try{
+        setIsLoading(true)
+        //scrape
+        const product=await scrapeAndStoreProduct(searchPrompt)
+    }
+    catch(error){
+        console.log(error)
+    }
+    finally{
+        setIsLoading(false)
+    }
     }
   return (
      <form
@@ -14,12 +54,15 @@ const Searchbar = () => {
      >
         <input
         type='text'
+         value={searchPrompt}
+         onChange={(e)=> setSearchPrompt(e.target.value)}
         placeholder='Enter the product link '
         className='searchbar-input'
         />
         <button type='submit'
+        disabled={searchPrompt===''}
         className='searchbar-btn'>
-            Search
+            {isLoading ? 'Searching...' :'Search'}
         </button>
      </form>
   )
