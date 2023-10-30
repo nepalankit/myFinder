7abbdf133069f55aa9ec2b9472fa5f15f0dc3beb
nepalankit <nepalankit16@gmail.com>
2023-10-30 23:01:58 +0545

    price sections

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -13,4 +13,4 @@ const scrapedProduct=await scrapeAmazonProduct(productUrl);
     catch(error:any){
         throw new Error(`Failed to create/update product :${error.message}`)
     }
-}Array
+}
--- lib/scraper/index.ts
+++ lib/scraper/index.ts
@@ -1,36 +1,81 @@
 import axios from "axios";
-import * as cheerio from 'cheerio'
-export async function scrapeAmazonProduct(url:string){
+import * as cheerio from "cheerio";
+import { extractCurrency, extractPrice } from "../utils";
+export async function scrapeAmazonProduct(url: string) {
+  if (!url) return;
 
-    if(!url) return;
+  //brightdata scarping to get product data
 
-    //brightdata scarping to get product data
-
-const username=String(process.env.BRIGHT_DATA_USERNAME)
-const password=String(process.env.BRIGHT_DATA_PASSWORD)
-const port=22225
-const session_id=(10000000*Math.random()) | 0
-const options={
-    auth:{
-        username:`${username}-session-${session_id}`,
-        password,
-
+  const username = String(process.env.BRIGHT_DATA_USERNAME);
+  const password = String(process.env.BRIGHT_DATA_PASSWORD);
+  const port = 22225;
+  const session_id = (10000000 * Math.random()) | 0;
+  const options = {
+    auth: {
+      username: `${username}-session-${session_id}`,
+      password,
     },
-    host:'brd.superproxy.io',
+    host: "brd.superproxy.io",
     port,
-    rejectUnauthorized:false,
-}
+    rejectUnauthorized: false,
+  };
 
+  try {
+    //fetch product
 
-try{
-        //fetch product
+    const response = await axios.get(url, options);
+    const $ = cheerio.load(response.data);
 
-const response=await axios.get(url,options)
-console.log(response)
-}
-catch(error:any)
-{
-    throw new Error(`failed to scrape product :${error.message}`)
-}
+    // extracting the product title
+    const title = $("#productTitle").text().trim();
 
+    //extract price
+    const currentPrice = extractPrice(
+      $(".priceToPay span.a-price-whole"),
+      $(".a.size.base.a-color-price"),
+      $(".a-button-selected .a-color-base")
+    );
+    //original price
+
+    const originalPrice = extractPrice(
+      $("#priceblock_ourprice"),
+      $(".a-price.a-text-price span.a-offscreen"),
+      $("#listPrice"),
+      $("#priceblock_dealprice"),
+      $(".a-size-base.a-color-price")
+    );
+
+    const outOfStock =
+      $("#availability span").text().trim().toLowerCase() ===
+      "currently unavailable";
+
+    const images =
+      $("#imgBlkFront").attr("data-a-dynamic-image") ||
+      $("#landingImage").attr("data-a-dynamic-image") ||
+      "{}"; //getting images and others
+
+    const imageUrls = Object.keys(JSON.parse(images)); //getting only images
+
+    const currency = extractCurrency($(".a-price-symbol"));
+
+    const discountRate = $(".savingsPercentage").text().replace(/[-%]/g, ""); // remove everthing that ain't percecntage
+
+    const data = {
+      url,
+      currency: currency || "$",
+      image: imageUrls[0],
+      title,
+      currentPrice: Number(currentPrice),
+      originalPrice: Number(originalPrice),
+      priceHistory: [],
+      discountRate: Number(discountRate),
+      category: "category",
+      reviews: 100,
+      stars: 4.7,
+      isOutOfStock: outOfStock,
+    };
+    console.log(data);
+  } catch (error: any) {
+    throw new Error(`failed to scrape product :${error.message}`);
+  }
 }
